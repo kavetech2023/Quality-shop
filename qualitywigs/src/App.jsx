a699fdb2086126a76cce5a304d40f3aaab5d80fe
kavetech2023 <kavetech@gmail.com>
2024-08-21 15:04:26 +0300

    chore: Initialize project with React, Vite, and Tailwind CSS

---
+++ qualitywigs/src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Collection from './Pages/Collection'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import Login from './Pages/Login'
+import PlaceOrder from './Pages/PlaceOrder'
+import Orders from './Pages/Orders'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/place-order" element={<PlaceOrder/>} />
+            <Route path="/orders" element={<Orders/>} />
+        </Routes>
+    </div>
+  )
+}
+
+export default App
