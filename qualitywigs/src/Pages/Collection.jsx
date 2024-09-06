b05f83561f7af912b0db5847c1d4f70dc1b0c8f5
kavetech2023 <kavetech@gmail.com>
2024-09-06 23:33:03 +0300

    feat: Add filter options to Collection page

--- qualitywigs/src/Pages/Collection.jsx
+++ qualitywigs/src/Pages/Collection.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/Context'
 
 const Collection = () => {
+
+  const {products} = useContext(ShopContext)
+  const [filter, setFilter] = useState(false)
+
   return (
-    <div>Collection</div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+        {/*Filter Options*/}
+        <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+
+
+        {/*Filter by Category*/}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${filter? "": "hidden"} sm:block`}>
+
+        </div>
+
+        </div>
+        
+    </div>
   )
 }
 
