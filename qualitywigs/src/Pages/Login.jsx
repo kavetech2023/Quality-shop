7516220ceb9bb67e55841664b568d302ee8ceb6b
kavetech2023 <kavetech@gmail.com>
2024-09-08 00:50:04 +0300

    feat: Add title component and update content in About and Contact pages

--- qualitywigs/src/Pages/Login.jsx
+++ qualitywigs/src/Pages/Login.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 
 const Login = () => {
   const [currentState, setCurrentState] = useState('Login')
+  const onSubmitHandler = async(e) => {
+    e.preventDefault()
+  }
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800 '>
         <div className='inline-flex items-center gap-2 mb-2 mt-10 '>
           <p className='prata-regular text-3xl'>{currentState}</p>
           <hr className='border-none h-[1.5px] w-8 bg-gray-800'/>
@@ -18,6 +21,7 @@ const Login = () => {
             currentState === 'Login' ? <p onClick={()=>setCurrentState('Register')} className='cursor-pointer'>Create an Account</p> : <p onClick={()=>setCurrentState('Login')} className='cursor-pointer'>Login</p>
           }
         </div>
+        <button className='bg-black w-full text-white font-light px-8 py-2 mt-4'>{currentState === 'Login'? 'Log In' : 'Register'}</button>
     </form>
   )
 }
